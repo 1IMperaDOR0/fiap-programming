107071b87f945309f3582c4da86a4d5b49493aa0
1IMperaDOR0 <lucaslucore@gmail.com>
2026-02-13 11:39:26 -0300

    Finished class of logic-java

---
+++ logic-java/02-13-26/App.java
@@ -0,0 +1,23 @@
+package br.com.fiap;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Explique Orientação à Objeto.");
+        System.out.println("");
+        System.out.println("Explique o que é objeto.");
+        System.out.println("");
+        System.out.println("Explique o que é classe.");
+        System.out.println("");
+        System.out.println("Explique o que é abstração.");
+        System.out.println("");
+        System.out.println("Explique o que é herança.");
+        System.out.println("");
+        System.out.println("Explique o que é encapsulamento.");
+        System.out.println("");
+        System.out.println("Explique o que é polimorfismo.");
+        System.out.println("");
+        System.out.println("Quais os principais tipos de polimorfismo? Explique cada um deles.");
+        System.out.println("");
+    }
+}
