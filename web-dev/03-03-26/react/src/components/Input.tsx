5dd659b6e7938673901dbe03deda7981b1997925
1IMperaDOR0 <lucaslucore@gmail.com>
2026-03-04 20:47:06 -0300

    Testing basic fundamentals

--- web-dev/03-03-26/react/src/components/Input.tsx
+++ web-dev/03-03-26/react/src/components/Input.tsx
@@ -8,11 +8,11 @@ export default function Input() {
             <h1>Input</h1>
             <input 
                 type="text" 
-                value={value.length > 20 ? "Valor máximo atingido" :value}
+                value={value}
                 onChange={(e) => setValue(e.target.value)} 
                 className={`border border-gray-300 rounded-md p-2 w-100 ${value.length > 20 ? "border-red-500 focus:outline-red-600 focus:outline-2" : ""}`}
             />
-            <p className={value.length > 20 ? "text-red-500" : ""}>Você está digitando: {value}</p>
+            <p className={value.length > 20 ? "text-red-500" : ""}>Você está digitando: {value.length > 20 ? "Valor máximo atingido" : value}</p>
         </div>
     )
 }
