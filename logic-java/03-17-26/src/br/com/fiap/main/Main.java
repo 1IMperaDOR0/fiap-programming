3f01febf89cdd6f59fc2ee54e3a62aabd56b33ca
1IMperaDOR0 <lucaslucore@gmail.com>
2026-03-17 10:29:17 -0300

    Finished class of logic-java

---
+++ logic-java/03-17-26/src/br/com/fiap/main/Main.java
@@ -0,0 +1,38 @@
+package br.com.fiap.main;
+
+import br.com.fiap.bean.Radio;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan;
+        Radio radio = new Radio();
+
+        int volume;
+        float estacao;
+
+        try {
+            // Leitura das informações
+            scan = new Scanner(System.in);
+            System.out.println("Digite um volume:");
+            volume = scan.nextInt();
+            System.out.println("Digite uma estação:");
+            estacao = scan.nextFloat();
+
+            // Atribuição dos valores para o objeto
+            radio.setVolume(volume);
+            radio.setEstacao(estacao);
+
+            // Aumentadno o volume (3x)
+            radio.aumentarVolume();
+            radio.aumentarVolume();
+            radio.aumentarVolume();
+
+            // Exibindo as informações
+            System.out.printf("Volume: %d\nEstação: %.1f", radio.getVolume(), radio.getEstacao());
+        } catch (Exception e) {
+            System.out.println("Formato incorreto!");
+        }
+    }
+}
